e1535c886218fd1d17186acebd2de6d4dce6e97d
123julianangarita <juliannnnnnnnnnnnnnnnnnn21@gmail.com>
2025-05-07 11:24:07 -0500

    Creo y configuro los metodos de get, post, put y delete en el modulo de programaModelo

--- SenaServerDeno/Models/conexion.ts
+++ SenaServerDeno/Models/conexion.ts
@@ -3,6 +3,6 @@ import { Client } from "../Dependencies/dependencias.ts";
 export const conexion = await new Client().connect({
     hostname: "localhost",
     username: "root",
-    db: "adso_mvc",
+    db: "sena_mvc",
     password: ""
 });
