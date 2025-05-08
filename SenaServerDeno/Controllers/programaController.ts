437f187014071dc41a8043e3a0652fb56e139685
123julianangarita <juliannnnnnnnnnnnnnnnnnn21@gmail.com>
2025-05-08 06:47:10 -0500

    Creo y el primer metodo del controlador de programa y corrijo metodos de post y delete en modelo programa

--- SenaServerDeno/Controllers/programaController.ts
+++ SenaServerDeno/Controllers/programaController.ts
@@ -1,6 +1,7 @@
 // deno-lint-ignore-file
 import { number } from "https://deno.land/x/zod@v3.24.1/types.ts";
 import { Programa } from "../Models/programaModelo.ts";
+import { errors } from "jsr:@oak/commons@^1.0/http_errors";
 
 export const getPrograma = async(ctx:any)=>{
     const { response } = ctx;
@@ -20,4 +21,133 @@ export const getPrograma = async(ctx:any)=>{
             errors: error
         }
     }
-}
+};
+
+export const postPrograma = async(ctx:any)=>{
+    const { response, request } = ctx;
+    try {
+        const contentLength = request.headers.get("Content-Length");
+
+        if (contentLength === 0) {
+            response.status = 400;
+            response.body = {success:false, msg:"El cuerpo de la solicitud se encuentra vacío."};
+            return;
+        }
+
+        const body = await request.body.json();
+        const ProgramaData = {
+            idPrograma: null,
+            nombre_programa: body.nombre_programa
+        }
+
+        const objPrograma = new Programa(ProgramaData)
+        const result = await objPrograma.InsertarPrograma();
+        response.status = 200;
+        response.body = {
+            success:true,
+            body: result
+        };
+
+    } catch (error) {
+        response.status = 400;
+        response.body = {
+            success:false,
+            msg:"Error al procesar la solicitud",
+            errors:error
+        }
+    }
+};
+
+export const putPrograma = async(ctx:any)=>{
+    const { response, request } = ctx;
+    try {
+        const contentLength = request.headers.get("Content-Length");
+        if (contentLength === 0) {
+            response.status = 400;
+            response.body = {success:false, msg:"El cuerpo pde la solicitud esta vacio"};
+            return;
+        }
+
+        const idPrograma = ctx.params.id;
+        if (!idPrograma) {
+            response.status = 400;
+            response.body = { success:false, msg:"Id no proporcionado"};
+            return;
+        }
+
+        const body = await request.body.json();
+        const ProgramaData = {
+            idPrograma:idPrograma,
+            nombre_programa:body.nombre_programa
+        }
+
+        const objPrograma = new Programa(ProgramaData)
+        const result = await objPrograma.ActualizarPrograma();
+        response.status = 200;
+        response.body = {
+            success:true,
+            body:result,
+            msg:"Programa no encontrado",
+        };
+
+    } catch (error) {
+        response.status = 400;
+        response.body = {
+            success:false,
+            msg:"Error al actualizar el programa",
+            errors:error
+        }        
+    }
+};
+
+export const deletePrograma = async(ctx:any)=>{
+    const { response, request } = ctx;
+
+    try {
+        const contentLength = request.headers.get("Content-Length");
+        if (!contentLength || Number(contentLength) === 0) {
+            response.status = 400;
+            response.body = { success:false, msg:"El cuerpo de la solicitud esta vacio"};
+            return;
+        }        
+
+        const body = await request.body.json();
+
+        if (!body.idPrograma) {
+            response.status = 400;
+            response.body = {
+                success:false,
+                msg:"id del programa no proporcionado"
+            };
+            return;
+        }
+
+        const ProgramaData = {
+            idPrograma: body.idPrograma,
+            nombre_programa:""
+        }
+
+        const objPrograma = new Programa(ProgramaData);
+        const result = await objPrograma.EliminarPrograma();
+
+        if (!result.success) {
+            response.status = 404;
+            response.body = {success:false, msg:result.message};
+            return;
+        }
+
+        response.status = 200;
+        response.body = {
+            success:true,
+            msg: result.message
+        };        
+
+    } catch (error) {
+        response.status = 400;
+        response.body = {
+            success:false,
+            msg:"Error al borrar el usuario",
+            errors:error
+        }        
+    }
+};
