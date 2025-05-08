1c4648868e0041adb069883d8815faaefd4c4461
Luis Salamanca <luisguezguann@gmail.com>
2025-05-08 07:29:05 -0500

    Creo el metodo delete en Controllers,Models y Routes

--- SenaServerDeno/Controllers/aprendizController.ts
+++ SenaServerDeno/Controllers/aprendizController.ts
@@ -1,5 +1,7 @@
 // deno-lint-ignore-file
+import { Body } from "https://deno.land/x/oak@v17.1.3/body.ts";
 import { Aprendiz } from "../Models/aprendizModelo.ts";
+import { Status } from 'https://deno.land/x/oak@v17.1.3/deps.ts';
 
 
 export const getAprendiz = async (ctx: any)=>{
@@ -112,3 +114,56 @@ export const putAprendiz = async(ctx: any)=>{
         }
     }
 }
+
+    export const deleteAprendiz  = async (ctx: any)=>{
+
+        const {response,request} = ctx;
+
+        try {
+
+            const contentLength = request.headers.get("Content-Length");
+
+            if (contentLength === "0") {
+                response.status = 400;
+                response.body = {success:false, msg:"El ID del usuario es requerido para eliminarlo"};
+                return;
+                
+            }
+
+            const body = await request.body.json();
+
+            if (!body.idaprendiz) {
+
+                response.status = 400;
+                response.body = {success:false, msg:"El ID del aprendiz es requerido para eliminarlo"};
+                return;
+                
+            }
+
+            const objAprendiz = new Aprendiz();
+            objAprendiz._objAprendiz = {
+                idaprendiz: body.idaprendiz,
+                nombre:"",
+                apellido:"",
+                email:"",
+                telefono:""
+
+            };
+
+            const result = await objAprendiz.EliminarAprendiz();
+
+            response.status = 200;
+            response.body = {
+                success:true,
+                body:result,
+            };
+            
+        } catch (error) {
+            response.status = 400;
+            response.body = {
+                success:false,
+                msg:"Error al procesar la solicitud"
+            }
+
+        }
+    }
--- SenaServerDeno/Routes/aprendizModels.ts
+++ SenaServerDeno/Routes/aprendizModels.ts
@@ -1,11 +1,12 @@
 import { Router } from "../Dependencies/dependencias.ts";
-import { getAprendiz,postAprendiz,putAprendiz} from "../Controllers/aprendizController.ts";
+import { getAprendiz,postAprendiz,putAprendiz,deleteAprendiz} from "../Controllers/aprendizController.ts";
 
 const routerAprendiz = new Router();
 
 routerAprendiz.get("/aprendiz",getAprendiz);
 routerAprendiz.post("/aprendiz",postAprendiz);
 routerAprendiz.put("/aprendiz",putAprendiz);
+routerAprendiz.delete("/aprendiz",deleteAprendiz);
 
 
 export {routerAprendiz}
